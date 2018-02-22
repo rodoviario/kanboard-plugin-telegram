755b85c78776206068bae27756401b7909f911af
linvinus <linvinus@gmail.com>
2018-02-22 11:51:50 +0300

    automation of receive chat_id

--- Template/project/integration.php
+++ Template/project/integration.php
@@ -1,4 +1,9 @@
 <h3><img src="<?= $this->url->dir() ?>plugins/Telegram/telegram-icon.png"/>&nbsp;Telegram</h3>
+    <?php $random = md5(time().$this->url->base().rand()) ?>
+
+    <p>Please send following message to the bot: <?= $random ?><br/>
+    then press <?= $this->modal->medium('none', t('Get chat id'), 'TelegramController', 'get_project_chat_id',array('plugin' => 'Telegram', 'private_message' => $random,'project_id' => $project['id'] ) ) ?>
+
 <div class="panel">
     <?= $this->form->label(t('Chat id of group chat with bot'), 'telegram_group_cid') ?>
     <?= $this->form->text('telegram_group_cid', $values, array()) ?>
--- Template/user/integration.php
+++ Template/user/integration.php
@@ -1,4 +1,9 @@
 <h3><img src="<?= $this->url->dir() ?>plugins/Telegram/telegram-icon.png"/>&nbsp;Telegram</h3>
+    <?php $random = md5(time().$this->url->base().rand()) ?>
+
+    <p>Please send following message to the bot: <?= $random ?><br/>
+    then press <?= $this->modal->medium('none', t('Get chat id'), 'TelegramController', 'get_user_chat_id',array('plugin' => 'Telegram', 'private_message' => $random )) ?>
+
 <div class="panel">
     <?= $this->form->label(t('Chat id of private chat with bot'), 'telegram_user_cid') ?>
     <?= $this->form->text('telegram_user_cid', $values) ?>
